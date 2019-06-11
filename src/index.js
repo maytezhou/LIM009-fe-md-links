678d46c4af1ebf3a51a7ada033c642a50a1144df
maytezhou <maytezhou14@gmail.com>
2019-06-11 15:06:52 -0500

    obtengoun array de objetos con su propiedad href y text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
+ // Create reference instance
+var myMarked = require('marked');
+
+// Get reference
+//var renderer = new myMarked.Renderer();
 
 
 import {
@@ -42,14 +47,14 @@ const gettingArrOfMarkdownFiles = (path2) => {
         arrOfFilesOrDirsInsideADir.forEach((filesOrDirs) => { // que  obtenga los elementos de la carpeta 
             const newPathAbsolute = absolutePath + '/' + filesOrDirs; // que  obtenga la ruta absoluta de cada uno de los elementos
             let newArr = gettingArrOfMarkdownFiles(newPathAbsolute);
-           //   console.log(newArr);
-           arrOfMarkdownFilesPath = arrOfMarkdownFilesPath.concat(newArr)
-           
+            //   console.log(newArr);
+            arrOfMarkdownFilesPath = arrOfMarkdownFilesPath.concat(newArr)
+
         });
 
 
     }
-   console.log(arrOfMarkdownFilesPath);
+    // console.log(arrOfMarkdownFilesPath);
     return arrOfMarkdownFilesPath;
 
 
@@ -58,24 +63,30 @@ const gettingArrOfMarkdownFiles = (path2) => {
 gettingArrOfMarkdownFiles(commandUser);
 
 
-const gettingArrObjOfMdLinks= (arrPaths) =>{
+const gettingArrObjOfMdLinks = (arrPaths) => {
     let arrObj = [];
-    arrPaths.forEach((filePath)=> {
+    // console.log(arrPaths);
+    arrPaths.forEach((filePath) => {
         const markdownContent = readFile(filePath).toString();
-      //  console.log(markdownContent)
-     
-    const url= 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)';
-        let a = markdownContent.search(url);
-       
-     })
-   
+        // console.log(markdownContent)
+        var renderer = new myMarked.Renderer();
+
+        renderer.link = (href, stats, text) => {
+            arrObj.push({ href, stats, text })
+
+        };
+        myMarked(markdownContent, { renderer: renderer });
+        console.log(arrObj);
+        return arrObj;
+    })
+
 };
 
-const mdLinks=(arrPaths)=> { 
-    arrPaths.forEach((filePath)=> {
-       let content=readFile(filePath);
+const mdLinks = (arrPaths) => {
+    arrPaths.forEach((filePath) => {
+        let content = readFile(filePath);
     })
 };
-gettingArrObjOfMdLinks(gettingArrOfMarkdownFiles(commandUser))
+gettingArrObjOfMdLinks(gettingArrOfMarkdownFiles(commandUser));
 // Ruta relativa de una carpeta '../archivos';
 // Ruta relativa de un archivo '../archivos/lucero.md';
--- src/links-obj.js
+++ src/links-obj.js
@@ -1,22 +1,27 @@
+// Create reference instance
+var myMarked = require('marked');
 
+// Get reference
+var renderer = new myMarked.Renderer();
 
 import {
     readFile
 } from './read-controller.js';
 
-const gettingArrObjOfMdLinks= (arrPaths) =>{
+const gettingArrObjOfMdLinks = (arrPaths) => {
     let arrObj = [];
-    arrPaths.forEach((filePath)=> {
+    arrPaths.forEach((filePath) => {
         const markdownContent = readFile(filePath).toString();
         console.log(markdownContent)
         var renderer = new myMarked.Renderer();
-       
-        renderer.link =(href, title,text)=>{
-            arrObj.push({href, text})
-            
-          };
-          myMarked(markdownContent,{renderer:renderer})
-            return arrObj;
-     })
-   
+
+        renderer.link = (href, text) => {
+            arrObj.push({ href, text })
+
+        };
+        myMarked(markdownContent, { renderer: renderer });
+        console.log(arrObj);
+        return arrObj;
+    })
+
 };
